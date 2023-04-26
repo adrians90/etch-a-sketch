cb0a54d2bd2674059d330fc87c91e7dbff15e2ff
adrians90 <adriansandu1990@yahoo.com>
2023-04-26 12:23:30 +0100

    etch a sketch works as expected

--- etch.js
+++ etch.js
@@ -3,6 +3,9 @@ const container = document.querySelector('.container');
 const sizeOfGrid = 16;
 
 const createGrid = (amtOfGrids) => {
+    const wrapper = document.createElement('div');
+    wrapper.classList.add('wrapper');
+
     for ( let i = 0; i < amtOfGrids; i++) {
         const row = document.createElement('div');
         row.classList.add('grid-row');
@@ -17,8 +20,24 @@ const createGrid = (amtOfGrids) => {
             row.appendChild(gridBox)
         }
 
-        container.appendChild(row);
+        wrapper.appendChild(row);
     }
+    container.appendChild(wrapper);
 }
 
 createGrid(sizeOfGrid);
+//added an event listener to the reset button that prompts user for input;
+resetButton.addEventListener('click', () => {
+    let userSize = Number(prompt('what dimensions do you want for the new grid?'));
+
+    while(userSize > 100) {
+        userSize = Number(prompt('Pick a number that is 100 or less'));
+    }
+
+    const wrapper = document.querySelector('.wrapper');
+    wrapper.remove();
+    createGrid(userSize);
+})
+
+
+
